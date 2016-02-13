0920c75bd1702c47fd78a9623fbe8897d6ba2a3f
Sahil Sareen <sahil.sareen@hotmail.com>
2016-02-13 17:23:08 +0530

    Add the States and Union territories of India

---
+++ src/States.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2016 Sahil Sareen (ssareen [AT] gnome [DOT] org)
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+# license.
+
+# Taken from https://en.wikipedia.org/wiki/States_and_union_territories_of_India
+
+states_in_india = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu", "Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
+]
---
+++ src/UnionTerritories.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2016 Sahil Sareen (ssareen [AT] gnome [DOT] org)
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+# license.
+
+# Taken from https://en.wikipedia.org/wiki/States_and_union_territories_of_India
+
+union_territories = [
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Lakshadweep",
+    "Delhi",
+    "Puducherry"
+]
